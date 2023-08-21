dc7235fe804b51c88b73c1815261b5d6afdff7d8
Cossiant <L861149396@163.COM>
2023-08-21 10:51:29 +0800

    V1.2.3 整合提交

--- Project/DebugProject/MDK-ARM/Debug_Function/Type/Debug.cpp
+++ Project/DebugProject/MDK-ARM/Debug_Function/Type/Debug.cpp
@@ -20,26 +20,72 @@ void Debug_Class::UART_Debug(const char *Data) {
 
 void Debug_Class::IO_Debug(char GPIO_PORT, int GPIO_PIN, unsigned char IO_Setting) {
     GPIO_TypeDef *Setting_GPIO_PORT;
-		if(GPIO_PORT=='A')
-		{
-			Setting_GPIO_PORT=GPIOA;
-		}
-		if(GPIO_PORT=='B')
-		{
-			Setting_GPIO_PORT=GPIOA;
-		}
-		if(GPIO_PORT=='C')
-		{
-			Setting_GPIO_PORT=GPIOA;
-		}
-		if(IO_Setting==1)
-		{
-			HAL_GPIO_WritePin(Setting_GPIO_PORT,GPIO_PIN,GPIO_PIN_SET);
-		}
-		if(IO_Setting==0)
-		{
-			HAL_GPIO_WritePin(Setting_GPIO_PORT,GPIO_PIN,GPIO_PIN_RESET);
-		}
+    int Setting_GPIO_PIN;
+    switch (GPIO_PIN) {
+        case (0):
+            Setting_GPIO_PIN = GPIO_PIN_0;
+            break;
+        case (1):
+            Setting_GPIO_PIN = GPIO_PIN_1;
+            break;
+        case (2):
+            Setting_GPIO_PIN = GPIO_PIN_2;
+            break;
+        case (3):
+            Setting_GPIO_PIN = GPIO_PIN_3;
+            break;
+        case (4):
+            Setting_GPIO_PIN = GPIO_PIN_4;
+            break;
+        case (5):
+            Setting_GPIO_PIN = GPIO_PIN_5;
+            break;
+        case (6):
+            Setting_GPIO_PIN = GPIO_PIN_6;
+            break;
+        case (7):
+            Setting_GPIO_PIN = GPIO_PIN_7;
+            break;
+        case (8):
+            Setting_GPIO_PIN = GPIO_PIN_8;
+            break;
+        case (9):
+            Setting_GPIO_PIN = GPIO_PIN_9;
+            break;
+        case (10):
+            Setting_GPIO_PIN = GPIO_PIN_10;
+            break;
+        case (11):
+            Setting_GPIO_PIN = GPIO_PIN_11;
+            break;
+        case (12):
+            Setting_GPIO_PIN = GPIO_PIN_12;
+            break;
+        case (13):
+            Setting_GPIO_PIN = GPIO_PIN_13;
+            break;
+        case (14):
+            Setting_GPIO_PIN = GPIO_PIN_14;
+            break;
+        case (15):
+            Setting_GPIO_PIN = GPIO_PIN_15;
+            break;
+    }
+    if (GPIO_PORT == 'A') {
+        Setting_GPIO_PORT = GPIOA;
+    }
+    if (GPIO_PORT == 'B') {
+        Setting_GPIO_PORT = GPIOB;
+    }
+    if (GPIO_PORT == 'C') {
+        Setting_GPIO_PORT = GPIOC;
+    }
+    if (IO_Setting == 1) {
+        HAL_GPIO_WritePin(Setting_GPIO_PORT, Setting_GPIO_PIN, GPIO_PIN_SET);
+    }
+    if (IO_Setting == 0) {
+        HAL_GPIO_WritePin(Setting_GPIO_PORT, Setting_GPIO_PIN, GPIO_PIN_RESET);
+    }
 }
 
 /*//测试代码
